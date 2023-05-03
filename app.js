644695c9fc7b784fcf7336f891e29c97b981ff6b
Gavin <gavinberetvas@ucsb.edu>
2023-05-03 08:19:20 -0700

    mobile navbar fix

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ let navbar = false;
 
 window.addEventListener("resize", function (event) {
   if (window.innerWidth > 800) {
-    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("sidebar").style.transform = "translateX(0%)";
   } else if (window.innerWidth < 800) {
-    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("sidebar").style.transform = "translateX(-100%)";
   }
 });
 
 function funnnction() {
-  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("sidebar").style.transform = "translateX(0%)";
   navbar = true;
 }
 
 function test2() {
   if (navbar != false) {
-    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("sidebar").style.transform = "translateX(-100%)";
   }
 }
